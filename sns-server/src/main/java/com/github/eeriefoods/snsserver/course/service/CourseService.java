33283c4c8027d4b79d6626ef0773ae518ca84c29
Jay Returns <jay3105@hotmail.com>
2022-06-13 14:54:41 +0200

    Added FriendlyName

--- sns-server/src/main/java/com/github/eeriefoods/snsserver/course/service/CourseService.java
+++ sns-server/src/main/java/com/github/eeriefoods/snsserver/course/service/CourseService.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Service
@@ -31,6 +32,8 @@ public class CourseService implements ICourseService {
 
     @Override
     public Course createCourse(Course course) {
+        course.setId(UUID.randomUUID().toString());
+
         return courseRepository.save(course);
     }
 
