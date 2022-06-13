3bbb5dcf703565ac9fa760e9cbb746aec073a8fa
AlexSchmitz222 <alexschmitz222@gmail.com>
2022-06-14 00:50:13 +0200

    Added a little bit of styling

--- sns-client/src/main/java/com/github/eeriefoods/snsclient/HelloApplication.java
+++ sns-client/src/main/java/com/github/eeriefoods/snsclient/HelloApplication.java
@@ -3,6 +3,7 @@ package com.github.eeriefoods.snsclient;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,8 +12,12 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Main.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 800);
+        stage.setTitle("DHBW Student Management Tool");
+        stage.getIcons().add(new Image("file:src/main/resources/assets/images/DHBWRaute.png"));
+        stage.setMinWidth(1300);
+        stage.setMinHeight(800);
+        stage.setMaximized(true);
         stage.setScene(scene);
         stage.show();
     }
