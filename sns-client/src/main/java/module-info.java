d1aee5622454ae414698dd057c71a5a44959ac2b
Rexphel <philiprexroth@gmail.com>
2022-06-15 14:49:52 +0200

    Refactor from Code. Factor out the Tables from the Main.fxml to StudentTable.fxml and CourseTable.fxml Same with Controller. I'm sorry Colin :x

--- sns-client/src/main/java/module-info.java
+++ sns-client/src/main/java/module-info.java
@@ -4,7 +4,11 @@ module com.github.eeriefoods.snsclient {
     requires com.google.gson;
     requires java.net.http;
 
-    opens com.github.eeriefoods.snsclient to javafx.fxml;
-    opens com.github.eeriefoods.snsclient.model to com.google.gson, javafx.base;
+    
     exports com.github.eeriefoods.snsclient;
+    exports com.github.eeriefoods.snsclient.controller;
+
+    opens com.github.eeriefoods.snsclient to javafx.fxml;
+    opens com.github.eeriefoods.snsclient.controller to javafx.fxml;
+    opens com.github.eeriefoods.snsclient.model to com.google.gson, javafx.base;
 }
