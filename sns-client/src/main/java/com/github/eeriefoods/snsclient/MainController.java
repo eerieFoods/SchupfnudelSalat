0f81aa6383e736ec2c0f3ca24a040659306e2ae3
Rexphel <philiprexroth@gmail.com>
2022-06-15 13:49:36 +0200

    Added & Fixed Comboboxen

--- sns-client/src/main/java/com/github/eeriefoods/snsclient/MainController.java
+++ sns-client/src/main/java/com/github/eeriefoods/snsclient/MainController.java
@@ -2,19 +2,16 @@ package com.github.eeriefoods.snsclient;
 
 import com.github.eeriefoods.snsclient.service.CourseService;
 import com.github.eeriefoods.snsclient.service.StudentService;
-import javafx.beans.binding.Bindings;
-import javafx.collections.FXCollections;
-import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.*;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 import com.github.eeriefoods.snsclient.model.*;
 import com.github.eeriefoods.snsclient.model.JavaLevel;
+import javafx.scene.control.cell.ComboBoxTableCell;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
 
@@ -22,16 +19,12 @@ public class MainController {
 
     @FXML private TableView<Student> TCS_View;
     @FXML private TableColumn<Student, Integer> TCS_ID;
-    @FXML private TableColumn<Student, String> TCS_FirstName;
-    @FXML private TableColumn<Student, String> TCS_LastName;
-    @FXML private TableColumn<Student, String> TCS_Course;
-    @FXML private TableColumn<Student, String> TCS_Company;
+    @FXML private TableColumn<Student, String> TCS_FirstName, TCS_LastName, TCS_Company;
     @FXML private TableColumn<Student, JavaLevel> TCS_JavaLevel;
-
+    @FXML private TableColumn<Student, String> TCS_Course;
     @FXML private TableView<Course> TCK_View;
     @FXML private TableColumn<Course, Integer> TCK_ID;
-    @FXML private TableColumn<Course, String> TCK_FriendlyName;
-    @FXML private TableColumn<Course, String> TCK_Room;
+    @FXML private TableColumn<Course, String> TCK_FriendlyName, TCK_Room;
 
     @FXML private void initialize() {
         
@@ -41,12 +34,22 @@ public class MainController {
         TCS_LastName.setCellValueFactory(new PropertyValueFactory<>("lastName"));
         TCS_LastName.setCellFactory(TextFieldTableCell.forTableColumn());
         TCS_JavaLevel.setCellValueFactory(new PropertyValueFactory<>("javaLevel"));
-//        TCS_JavaLevel.setCellFactory(TextFieldTableCell.forTableColumn());
+        TCS_JavaLevel.setCellFactory(ComboBoxTableCell.forTableColumn(JavaLevel.values()));
         TCS_Company.setCellValueFactory(new PropertyValueFactory<>("company"));
         TCS_Company.setCellFactory(TextFieldTableCell.forTableColumn());
         TCS_Course.setCellValueFactory(new PropertyValueFactory<>("courseId"));
-        TCS_Course.setCellFactory(TextFieldTableCell.forTableColumn());
-        TCS_View.getItems().setAll(parseStudentList());
+        try { // TODO Error Handling
+            String[] courses = CourseService
+                    .getCourses()
+                    .stream()
+                    .map(Course::getFriendlyName)
+                    .toArray(String[]::new);
+            
+            TCS_Course.setCellFactory(ComboBoxTableCell.forTableColumn(courses));
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        TCS_View.getItems().setAll(loadStudentList());
 
         TCK_ID.setCellValueFactory(new PropertyValueFactory<>("id"));
         TCK_FriendlyName.setCellValueFactory(new PropertyValueFactory<>("friendlyName"));
@@ -64,23 +67,14 @@ public class MainController {
             updateStudent(event.getRowValue());});
         TCS_Course.setOnEditCommit(event -> {
             event.getTableView().getItems().get(event.getTablePosition().getRow()).setCourse(event.getNewValue());
-            updateStudent(event.getRowValue());});
-
-        TCS_View.setEditable(true);
-
-        TCS_JavaLevel.setCellValueFactory(i -> {
-            final JavaLevel javaLevel = i.getValue().getJavaLevel();
-            // binding to constant value
-            return Bindings.createObjectBinding(() -> javaLevel);
+            updateStudent(event.getRowValue());
+        });
+        TCS_JavaLevel.setOnEditCommit(event -> {
+            event.getTableView().getItems().get(event.getTablePosition().getRow()).setJavaLevel(event.getNewValue());
+            updateStudent(event.getRowValue());
         });
 
-        TCS_JavaLevel.setCellFactory(col -> {
-            TableCell<Student, JavaLevel> c = new TableCell<>();
-            final ComboBox<JavaLevel> comboBox = new ComboBox<>();
-            comboBox.setItems(FXCollections.observableArrayList( JavaLevel.values()));
-            c.graphicProperty().bind(Bindings.when(c.emptyProperty()).then((Node) null).otherwise(comboBox));
-            return c;});
-
+        TCS_View.setEditable(true);
     }
 
     private void updateStudent(Student student){
