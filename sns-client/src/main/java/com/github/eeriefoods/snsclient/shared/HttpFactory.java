fa871e5f49a95a2e65ec26777c49b03eb3d2e310
AlexSchmitz222 <alexschmitz222@gmail.com>
2022-06-22 23:57:30 +0200

    Changes by jay31

--- sns-client/src/main/java/com/github/eeriefoods/snsclient/shared/HttpFactory.java
+++ sns-client/src/main/java/com/github/eeriefoods/snsclient/shared/HttpFactory.java
@@ -5,6 +5,7 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.List;
 
 public final class HttpFactory {
 
