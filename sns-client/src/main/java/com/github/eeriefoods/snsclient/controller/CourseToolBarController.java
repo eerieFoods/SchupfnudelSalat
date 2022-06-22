9efe4a8bba2ab1481cb60a623bdc1b421615c8b2
AlexSchmitz222 <alexschmitz222@gmail.com>
2022-06-22 12:23:14 +0200

    did stuffs

--- sns-client/src/main/java/com/github/eeriefoods/snsclient/controller/CourseToolBarController.java
+++ sns-client/src/main/java/com/github/eeriefoods/snsclient/controller/CourseToolBarController.java
@@ -19,7 +19,7 @@ public class CourseToolBarController extends ToolBar{
 
         BTNDelete.setOnAction(event -> {
             NotificationHandler nh = new NotificationHandler();
-            if (nh.askForConfirmation("Wirklich löschen?", "Im Kurs " + courseTableView.getSelectionModel().getSelectedItem().getId() + " befinden sich noch " + " Student:innen. Soll der Kurs wirklich gelöscht werden?")) {
+            if (nh.askForConfirmation("Wirklich löschen?", "Im Kurs " + courseTableView.getSelectionModel().getSelectedItem().getId() + " befinden sich noch" + " Student:innen. Soll der Kurs wirklich gelöscht werden?")) {
                 Course course = courseTableView.getSelectionModel().getSelectedItem();
                 courseTableController.deleteCourse(course);
                 courseTableView.getItems().remove(course);
